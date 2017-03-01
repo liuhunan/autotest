68c5ffcb790400d990a72084e982bece4124b478
hunan.liu <704769524@qq.com>
2017-03-01 18:52:25 +0800

    uiwatch

--- app/src/androidTest/java/com/example/hunanliu/autotest/UiWatchTest.java
+++ app/src/androidTest/java/com/example/hunanliu/autotest/UiWatchTest.java
@@ -19,57 +19,66 @@ import org.junit.runner.RunWith;
 public class UiWatchTest {
     Instrumentation instrumentation;
     UiDevice device;
-    @Before
-    public  void steup(){
-        UiWatcher watcher=new UiWatcher() {
-            @Override
-            public boolean checkForCondition() {
-                UiObject2 smallscreen = device.findObject(By.res("android:id/title"));
-                UiObject2 fullscreen = device.findObject(By.res("glow_pad_view"));
-                if (smallscreen != null) {
-                    smallscreen.click();
-                    try {
-                        sleep(10000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    UiObject2 endcall = device.findObject(By.res("com.android.dialer:id/floating_end_call_action_button"));
-                    endcall.click();
-                    try {
-                        sleep(3000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    return true;
+    public void UiWatchTest(){UiWatcher watcher=new UiWatcher() {
+        @Override
+        public boolean checkForCondition() {
+            UiObject2 smallscreen = device.findObject(By.res("android:id/title"));
+            UiObject2 fullscreen = device.findObject(By.res("glow_pad_view"));
+            if (smallscreen != null) {
+                smallscreen.click();
+                try {
+                    sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
-                return false;
+                UiObject2 endcall = device.findObject(By.res("com.android.dialer:id/floating_end_call_action_button"));
+                endcall.click();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return true;
             }
-        };
+
+            return false;
+        }
+
+        @Before
+        public void steup() throws InterruptedException {
+            device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+            device.pressBack();
+            sleep(1000);
+            device.pressBack();
+            sleep(1000);
+            device.pressHome();
+            sleep(3000);
+
+        }
+
+        ;
 
 
-    }
-
-    @Test
-    public void TestComiingCall() throws InterruptedException {
-        UiObject2 phone=device.findObject(By.desc("Phone"));
+        @Test
+        public void TestComiingCall() throws InterruptedException {
+            UiObject2 phone = device.findObject(By.desc("Phone"));
 //        System.out.println(phone.getText());
-        while (true){
-        device.pressBack();
-        sleep(500);
-        device.pressBack();
-        sleep(500);
-        device.pressHome();
-        sleep(2000);
-        phone.click();
-        sleep(3000);
-        UiObject2 one=device.findObject(By.res("com.android.dialer:id/dialpad_key_number"));
-        one.click();
+            while (true) {
+                device.pressBack();
+                sleep(500);
+                device.pressBack();
+                sleep(500);
+                device.pressHome();
+                sleep(2000);
+                phone.click();
+                sleep(3000);
+                UiObject2 one = device.findObject(By.res("com.android.dialer:id/dialpad_key_number"));
+                one.click();
 
-    }}
-    public void sleep(int time) throws InterruptedException {
-        Thread.sleep(time);
+            }
+        }
+
+        public void sleep(int time) throws InterruptedException {
+            Thread.sleep(time);
+        }
     }
-    }
-
-
