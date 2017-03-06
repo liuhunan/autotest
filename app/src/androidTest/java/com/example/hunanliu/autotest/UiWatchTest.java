7477722bb21fdb5627188cbf01f14755ff2d2ae4
hunan.liu <704769524@qq.com>
2017-03-06 12:15:41 +0800

    uiwatch

--- app/src/androidTest/java/com/example/hunanliu/autotest/UiWatchTest.java
+++ app/src/androidTest/java/com/example/hunanliu/autotest/UiWatchTest.java
@@ -7,7 +7,9 @@ import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.UiWatcher;
+import android.util.Log;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,40 +21,13 @@ import org.junit.runner.RunWith;
 public class UiWatchTest {
     Instrumentation instrumentation;
     UiDevice device;
-    public void UiWatchTest(){UiWatcher watcher=new UiWatcher() {
-        @Override
-        public boolean checkForCondition() {
-            UiObject2 smallscreen = device.findObject(By.res("android:id/title"));
-            UiObject2 fullscreen = device.findObject(By.res("glow_pad_view"));
-            if (smallscreen != null) {
-                smallscreen.click();
-                try {
-                    sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                UiObject2 endcall = device.findObject(By.res("com.android.dialer:id/floating_end_call_action_button"));
-                endcall.click();
-                try {
-                    sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                return true;
-            }
-
-            return false;
-        }
+    public void sleep(int time) throws InterruptedException {
+        Thread.sleep(time);
+    }
 
         @Before
         public void steup() throws InterruptedException {
             device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-            device.pressBack();
-            sleep(1000);
-            device.pressBack();
-            sleep(1000);
-            device.pressHome();
-            sleep(3000);
 
         }
 
@@ -60,16 +35,21 @@ public class UiWatchTest {
 
 
         @Test
+
         public void TestComiingCall() throws InterruptedException {
-            UiObject2 phone = device.findObject(By.desc("Phone"));
+            UiObject2 phone = device.findObject(By.text("Phone"));
 //        System.out.println(phone.getText());
             while (true) {
+                String TAG="liuhunan";
+
+
                 device.pressBack();
                 sleep(500);
                 device.pressBack();
                 sleep(500);
                 device.pressHome();
-                sleep(2000);
+                sleep(3000);
+
                 phone.click();
                 sleep(3000);
                 UiObject2 one = device.findObject(By.res("com.android.dialer:id/dialpad_key_number"));
@@ -78,7 +58,5 @@ public class UiWatchTest {
             }
         }
 
-        public void sleep(int time) throws InterruptedException {
-            Thread.sleep(time);
-        }
+
     }
